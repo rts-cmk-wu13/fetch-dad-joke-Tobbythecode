586f74a66d04a6d8a33e8899b815be72436553b0
Tobias <tobimation23@gmail.com>
2025-02-06 12:48:22 +0100

    made it print it out on html

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ headers: {
 }
 })
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data =>  {
+
+    let bodyElm = document.querySelector("body");
+
+    let divElm = document.createElement("div")
+
+    divElm.innerHTML = `
+    <h1>${data.joke}</h1>
+
+
+    `
+bodyElm.append(divElm)
+})
